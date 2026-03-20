0a60c4b81e838782b85851c634e378d7120bd07c
Poon Wei Xuan <poonweixuan@gmail.com>
2026-03-21 00:28:36 +0800

    Implement launcher and fairshare main

--- src/main/java/fairshare/FairShare.java
+++ src/main/java/fairshare/FairShare.java
@@ -1,5 +1,30 @@
 package fairshare;
 
-public class FairShare {
+import fairshare.logic.Logic;
+import fairshare.logic.LogicManager;
+import fairshare.model.Model;
+import fairshare.model.ModelManager;
+import fairshare.ui.Ui;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+/**
+ * Main entry point to FairShare
+ */
+public class FairShare extends Application {
+    private Ui ui;
+    private Logic logic;
+    private Model model;
+    private Storage storage;
+
+    public void init() throws Exception {
+        super.init();
+        this.model = new ModelManager();
+        this.logic = new LogicManager(model, storage);
+        this.ui = new UiManager(logic);
+    }
+
+    public void start(Stage stage) {
+        ui.start(stage);
+    }
 }
--- src/main/java/fairshare/logic/LogicManager.java
+++ src/main/java/fairshare/logic/LogicManager.java
@@ -6,22 +6,24 @@ import fairshare.logic.commands.exceptions.CommandException;
 import fairshare.logic.parser.FairShareParser;
 import fairshare.logic.parser.exceptions.ParseException;
 import fairshare.model.Model;
+import fairshare.storage.Storage;
 
 public class LogicManager implements Logic {
     private FairShareParser fairShareParser;
     private Model model;
-    // private Storage storage;
+    private Storage storage;
 
-    public LogicManager(Model model) {
+    public LogicManager(Model model, Storage storage) {
         this.fairShareParser = new FairShareParser();
         this.model = model;
+        this.storage = storage;
     }
 
     public CommandResult execute(String userInput) throws ParseException, CommandException{
         Command cmd = fairShareParser.parseCommand(userInput);
         CommandResult cmdResult = cmd.execute(model);
 
-        // storage.saveExpenseList
+        storage.saveExpenseList
 
         return cmdResult;
     }
